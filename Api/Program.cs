0557634c348e64df6a0b2f33dedc1785cc3cbf4c
Meirilis <meirilis@outlook.com>
2026-06-03 08:59:47 -0300

    chore: configura autenticacao bearer no swagger

--- Api/Program.cs
+++ Api/Program.cs
@@ -4,6 +4,7 @@ using Api.Helpers;
 using Api.Middlewares;
 using Api.Security.Jwt;
 using DotNetEnv;
+using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 Env.Load();
@@ -48,7 +49,33 @@ builder.Services.AddCors(options =>
     );
 });
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Informe o token JWT no formato: Bearer {seu token}"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+});
 
 var app = builder.Build();
 
